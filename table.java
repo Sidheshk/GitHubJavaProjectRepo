25f8c9d38d54fb147b6f9228c62e9f7f3fd60397
Sidhesh Kesarkar <kesarkarsidhesh@gmail.com>
2022-12-26 20:40:50 +0530

    first commit

---
+++ table.java
@@ -0,0 +1,14 @@
+
+public class table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a=5;
+			
+			for(int i=0;i<=10;i++)
+			{
+				System.out.println( a +" * "+i+" = "+a*i);
+			}
+	}
+
+}
